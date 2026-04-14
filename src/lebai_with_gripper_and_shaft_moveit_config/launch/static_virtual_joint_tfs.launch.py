3ca7052d386b126cf629444e21d5fc64806d93dd
shenyouyi123-ux <shenyouyi123@gmail.com>
2026-04-14 22:35:26 +0800

    feat:moveit包含shaf、gripper,生成moveit_config包

---
+++ src/lebai_with_gripper_and_shaft_moveit_config/launch/static_virtual_joint_tfs.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("lm3_with_shaft", package_name="lebai_with_gripper_and_shaft_moveit_config").to_moveit_configs()
+    return generate_static_virtual_joint_tfs_launch(moveit_config)
